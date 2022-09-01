251b02577dcc0864fae71368d5a017a6c7603641
Srivatsav R <80978172+Srivatsav1801@users.noreply.github.com>
2022-09-01 12:51:05 +0530

    Add files via upload

--- python0/week2/Twttr.py
+++ python0/week2/Twttr.py
@@ -1,7 +1,13 @@
-string = input("Input:")
-for q in string:
-    if q not in ["a","e","i","o","u","A","E","I","O","U"]:
-        {
-            print(q,end="")
-        }
-    
+def main():
+    word = input("Input:")
+    omit(word)
+
+
+def omit(word):
+    for _ in word:
+        if _ not in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]:
+            print(_, end="")
+
+
+if __name__ == '__main__':
+    main()
