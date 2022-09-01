a1c35dac9f4b20d9fcc7d5ef18abaaa86a6c7b82
Srivatsav R <80978172+Srivatsav1801@users.noreply.github.com>
2022-09-01 12:15:26 +0530

    Add files via upload

--- python0/week1/interpreter.py
+++ python0/week1/interpreter.py
@@ -1,16 +1,26 @@
-x,y,z = input("Expression:").split(" ")
-x = float(x)
-z = float(z)
-if y == "+":
-    ans = x + z
+def main():
+    expr = input("Expression:")
+    ans = calc(expr)
     print(ans)
-elif y == "-":
-    ans = x - z
-    print(ans)
-elif y == "/":
-    ans = x / z
-    print(ans)
-elif y == "*":
-    ans = x*z 
-    print(ans)
-   
+
+
+def calc(expr):
+    x, y, z = expr.split(" ")
+    x = float(x)
+    z = float(z)
+    if y == "+":
+        ans = x + z
+        return ans
+    elif y == "-":
+        ans = x - z
+        return ans
+    elif y == "/":
+        ans = x / z
+        return ans
+    elif y == "*":
+        ans = x * z
+        return ans
+
+
+if __name__ == '__main__':
+    main()
