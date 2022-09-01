a9a4f14f618e88603ab6b953d66db6eef9eee4e8
Srivatsav R <80978172+Srivatsav1801@users.noreply.github.com>
2022-09-01 12:26:15 +0530

    Add files via upload

--- python0/week1/meal.py
+++ python0/week1/meal.py
@@ -1,20 +1,19 @@
 def main():
-    time = str(input("What time is it?"))
+    time = str(input("What time is it? "))
     ml = convert(time)
-    if 8.00 >= ml >= 7.00:
+    if 7.00 <= ml <= 8.00:
         print("breakfast time")
-    elif 13.00 >= ml <= 12.00:
+    elif 12.00 <= ml <= 13.00:
         print("lunch time")
-    elif 19.00 >= ml <=18.00:
+    elif 18.00 <= ml <=19.00:
         print("dinner time")
     else:
-        print("work time") 
+        return 0
 
 
 def convert(time):
     hrs, min = time.split(":")
     hours = int(hrs) + (int(min)/60)
-    print(hours)
     return hours 
     
 
