74c2b7ff62abe2a05deccceea7bb9ecfd2b9a36e
Srivatsav R <80978172+Srivatsav1801@users.noreply.github.com>
2022-09-01 14:17:00 +0530

    Add files via upload

--- python0/week3/fuel.py
+++ python0/week3/fuel.py
@@ -1,19 +1,27 @@
-while True:
-    try:
-        n,m = input("Fraction:").split("/")
-        f = int(n)/int(m)
-        if f<=1:
+def main():
+    amt = input("Fraction:")
+    fuel(amt)
+
+
+def fuel(amt):
+    while True:
+        try:
+            n, m = amt.split("/")
+            f = int(n) / int(m)
+            if n > m:
+                continue
             break
-        if n>m:
-            print("ValueError")
-    except(ValueError,ZeroDivisionError):
-        pass
-    
-p = f * 100
-if p <=1:
-    print("E")
-elif p>=99:
-    print("F")
-else:
-    print("{:.2f}%".format(p))
-    
+        except(ValueError, ZeroDivisionError):
+            continue
+
+    p = f * 100
+    if p <= 1:
+        print("E")
+    elif p >= 99:
+        print("F")
+    else:
+        print("{:.2f}%".format(p))
+
+
+if __name__ == '__main__':
+    main()
