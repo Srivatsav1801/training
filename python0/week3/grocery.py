2533e6fdf2f74626b6ae318a71a3b7b1d833f3c0
Srivatsav R <80978172+Srivatsav1801@users.noreply.github.com>
2022-09-01 14:28:16 +0530

    Add files via upload

--- python0/week3/grocery.py
+++ python0/week3/grocery.py
@@ -1,12 +1,21 @@
-list = {}
-while True:
-    try:
-        item = input()
+def main():
+    grocery()
+
+
+def grocery():
+    list = {}
+    while True:
+        try:
+            item = input()
+        except EOFError:
+            for item in sorted(list.keys()):
+                print(list[item], item)
+            break
         if item in list:
             list[item] += 1
         else:
             list[item] = 1
-    except EOFError:
-        for key in sorted(list.keys()):
-            print(list[key],key)
-            
+
+
+if __name__ == '__main__':
+    main()
