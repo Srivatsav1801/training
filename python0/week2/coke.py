407190b71172a9332e6fdb6a5f3f277bf2986a65
Srivatsav R <80978172+Srivatsav1801@users.noreply.github.com>
2022-09-01 12:44:22 +0530

    Add files via upload

--- python0/week2/coke.py
+++ python0/week2/coke.py
@@ -1,7 +1,16 @@
-cokeprice = 50 
-while cokeprice > 0:
-    print(f"Amount Due:{cokeprice}")
-    coin = int(input("Insert Coin"))
-    cokeprice = cokeprice - coin
-    if cokeprice <= 0 :
-        print(f"Change owned:{cokeprice*(-1)}") 
+def main():
+    cokeprice = 50
+    pcheck(cokeprice)
+
+
+def pcheck(cokeprice):
+    while cokeprice > 0:
+        print(f"Amount Due:{cokeprice}")
+        coin = int(input("Insert Coin"))
+        cokeprice = cokeprice - coin
+        if cokeprice <= 0:
+            print(f"Change owned:{cokeprice * (-1)}")
+
+
+if __name__ == '__main__':
+    main()
