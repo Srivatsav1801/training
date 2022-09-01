f095fbc62ad0f22ec12796048565197a6063ba07
Srivatsav R <80978172+Srivatsav1801@users.noreply.github.com>
2022-09-01 11:48:38 +0530

    Add files via upload

--- python0/week1/Deepthought.py
+++ python0/week1/Deepthought.py
@@ -1,5 +1,19 @@
-no=str(input("What is the Answer to the Great Question of Life, the Universe, and Everything ?")).lower()
-if(no == "42" or no == "forty-two" or no == "forty two"):
-    print("Yes")
-else:
-    print("No")
+def main():
+    ans = str(input("What is the Answer to the Great Question of Life, the Universe, and Everything ?")).lower()
+    re = check(ans)
+    print(re)
+
+
+def check(ans):
+    if ans == "42":
+        return "Yes"
+    elif ans == "forty-two":
+        return "Yes"
+    elif ans == "forty-two":
+        return "Yes"
+    else:
+        return "No"
+
+
+if __name__ == '__main__':
+    main()
