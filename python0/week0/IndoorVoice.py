57297c17419c13feb21b589a1ec0d28f0fedc02b
Srivatsav R <80978172+Srivatsav1801@users.noreply.github.com>
2022-09-01 11:02:50 +0530

    Add files via upload

--- python0/week0/IndoorVoice.py
+++ python0/week0/IndoorVoice.py
@@ -1,2 +1,15 @@
-string = input().lower()
-print(string)
+
+from ast import main
+
+
+def main():
+    msg = input("Enter the Message:")
+    nmsg = cap_to_low(msg)
+    print(f"{nmsg}")
+    
+def cap_to_low(msg):
+    low_msg = msg.lower()
+    return low_msg
+
+main()
+
