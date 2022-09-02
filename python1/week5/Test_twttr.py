6c48cf7a484697f0112f3a4921bbe66b3a5e32b1
Srivatsav R <80978172+Srivatsav1801@users.noreply.github.com>
2022-09-02 18:50:54 +0530

    Add files via upload

---
+++ python1/week5/Test_twttr.py
@@ -0,0 +1,12 @@
+from Twttr import shorten
+def main():
+    test_twttr()
+
+
+def test_twttr():
+    assert shorten("hello") == "hll"
+    assert shorten("HELLO") == "HLL"
+    assert shorten("Hello World") =="Hll Wrld"
+
+if __name__ == '__main__':
+    main()
