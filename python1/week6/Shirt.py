30efd59195cda65c59b77a0d9912838d0ae0ce00
Srivatsav R <80978172+Srivatsav1801@users.noreply.github.com>
2022-09-05 17:00:59 +0530

    Add files via upload

---
+++ python1/week6/Shirt.py
@@ -0,0 +1,29 @@
+import sys
+from PIL import Image, ImageOps
+import os
+if len(sys.argv) == 3:
+    extension = ['.jpg','.jpeg','png']
+    extension1 = os.path.splitext(sys.argv[1])
+    extension2 = os.path.splitext(sys.argv[2])
+    if extension[1] == extension[1] and extension[1] in extension:
+        try:
+            uimage = Image.open(sys.argv[1])
+        except FileNotFoundError:
+            sys.exit("File doesn't exist")
+        shirt = Image.open("shirt.png")
+        size = shirt.size
+        uimage = ImageOps.fit(uimage, size)
+        uimage.paste(shirt, shirt)
+        uimage.save(sys.argv[2])
+    elif extension1[1] != extension2[1]:
+        sys.exit("Input and output have different extensions")
+    else:
+        sys.exit("Wrong extension")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command line arguments")
+elif len(sys.argv) < 3:
+    sys.exit("Too few command line arguments")
+
+
+
+
